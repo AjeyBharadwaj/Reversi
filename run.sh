eaaa10be2bacbcd3a17ba498ccb99381be66c2c7
AjeyBharadwaj <chi.ajeyanantharamaiah@gmail.com>
2016-07-14 14:40:10 +0530

    Working

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm -rf possiblity.log
 ./generate_possiblity < start.log > possiblity.log
 rm -rf move_done.log
@@ -5,7 +6,7 @@ rm -rf move_done.log
 rm -rf final.log
 ./modify possiblity.log move_done.log > final.log
 
-for VARIABLE in 1 2 3 4 5 
+for i in `seq 1 95`;
 do
 rm -rf possiblity.log
 ./generate_possiblity < final.log > possiblity.log
